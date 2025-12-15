9c46abbb3846a60436e6a5a6edb6ef7e65a29503
murphy_955 <13776528859@163.com>
2025-12-15 21:25:24 +0800

    初始化微服务后端项目

---
+++ backend/player-stats-service/src/main/java/cn/edu/fzu/oopdesign/zeyuli/PlayerStatsServiceApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.fzu.oopdesign.zeyuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerStatsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerStatsServiceApplication.class, args);
+    }
+}

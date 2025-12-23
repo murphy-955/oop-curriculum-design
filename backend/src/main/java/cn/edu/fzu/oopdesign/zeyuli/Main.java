e3dabacc5275fbf43a4f2f0ca484508305db724a
murphy_955 <13776528859@163.com>
2025-12-23 14:37:38 +0800

    src: 启动类

---
+++ backend/src/main/java/cn/edu/fzu/oopdesign/zeyuli/Main.java
@@ -0,0 +1,11 @@
+package cn.edu.fzu.oopdesign.zeyuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
